803544eccaa2b729bccb70b0e8b4ba8a4e733336
D4rkgh057 <danielfuelpaz@outlook.com>
2024-05-02 06:39:52 -0500

    UserPage and api finished

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,15 +1,64 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import "./User.css";
+import { Loader } from "../components";
+import { TbPencilCog } from "react-icons/tb";
+import { PokemonContext } from "../context/PokemonContext";
+import imgholder from "../assets/pokeball.svg";
 
 export const UserPage = () => {
-  const username = localStorage.getItem('username');
+  const { allPokemons } = useContext(PokemonContext);
+  const username = localStorage.getItem("username");
   const navigate = useNavigate();
 
   const [teams, setTeams] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null); 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [editablePokemon, setEditablePokemon] = useState({}); // Add this line to initialize the state with an empty object
+  const [visible, setVisible] = useState(false); // Add this line to initialize the state with a boolean value [false
+  const [editIndex, setEditIndex] = useState(0); // Add this line to initialize the state with a null value [null
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value); // Update search term on input change
+  };
+
+  const handleEditPokemon = (pokemon, index) => {
+    setEditablePokemon(pokemon);
+    setEditIndex(index + 1);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission
+
+    setVisible(true);
+    // Implement your search logic here using searchTerm
+    filterPokemon(searchTerm); // Assuming filterPokemon function takes searchTerm as input
+  };
+
+  function formatString(str) {
+    return str
+      .replace(/-/g, " ")
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+
+  const filterPokemon = () => {
+    const filteredPokemon = allPokemons.find((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (filteredPokemon) {
+      setEditablePokemon({
+        id: filteredPokemon.id,
+        name: filteredPokemon.name,
+        image: filteredPokemon.sprites.other["official-artwork"].front_default,
+      });
+    } else {
+      setEditablePokemon({}); // Reset editablePokemon if no match found
+    }
+  };
 
   const cargarEquipos = () => {
     fetch(`http://localhost:5000/team?username=${username}`, {
@@ -23,20 +72,112 @@ export const UserPage = () => {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error("Error en la petición");
+          throw new Error("Error on request");
         }
       })
       .then((data) => {
         // Guarda los datos en el estado
         setTeams(data.teams); // Asegúrate de que estás accediendo a la propiedad correcta del objeto de respuesta
         setIsLoading(false); // Actualiza el estado de carga después de recibir los datos
-        console.log(data);
       })
       .catch((error) => {
         console.error("Error:", error);
-        setError(error.toString()); // Actualiza el estado de error en caso de que ocurra un error
         setIsLoading(false); // Actualiza el estado de carga en caso de que ocurra un error
       });
+  };
+
+  const getPokemon = async (id) => {
+    // Construir la URL para la solicitud GET
+    const url = `http://localhost:5000/pokemon?id=${id}`;
+
+    try {
+      // Realizar la solicitud GET y esperar la respuesta
+      const response = await fetch(url);
+
+      // Verificar si la solicitud fue exitosa
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Parsear la respuesta como JSON
+      const data = await response.json();
+      // Devolver los resultados
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
+  };
+
+  const handleAddPokemon = async () => {
+    try {
+      // Check if the Pokémon already exists in the database
+      const existingPokemon = await getPokemon(editablePokemon.id);
+
+      // If the Pokémon already exists, log a message and exit the function
+      if (existingPokemon.length > 0) {
+        return;
+      }
+
+      // If the Pokémon doesn't exist, perform the insertion
+      const response = await fetch(`http://localhost:5000/pokemon`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+        body: JSON.stringify(editablePokemon), // Use editablePokemon for the new Pokémon data
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleUpdatePokemonOnTeam = async (
+    teamId,
+    pokemonPosition,
+    newPokemonId
+  ) => {
+    try {
+      await handleAddPokemon();
+      const response = await fetch(`http://localhost:5000/team`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+        body: JSON.stringify({ teamId, pokemonPosition, newPokemonId }),
+      });
+
+      if (response.ok) {
+        cargarEquipos();
+        setVisible(false);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDeletePokemonFromTeam = async (teamId, pokemonPosition) => {
+    try {
+      const response = await fetch(`http://localhost:5000/team/pokemon`, {
+        method: "PUT", // Cambiar el método a PUT
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+        body: JSON.stringify({ teamId, pokemonPosition }), // Utilizar el cuerpo para enviar los datos
+      });
+
+      if (response.ok) {
+        cargarEquipos();
+        setVisible(false);
+      } else {
+        console.error("Failed to delete Pokemon from team");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   if (!username) {
@@ -58,30 +199,132 @@ export const UserPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="title">Welcome, {username}!</h1>
-      <p>This is your user page.</p>
+    <div className="user-container">
+      <div className="user-profile">
+        <h1 className="title" style={{ textAlign: "center" }}>
+          Welcome to your Pokémon Trainer page,<br></br> {username}!
+        </h1>
+        <p>
+          Here you can manage your Pokémon teams, add new Pokémon to your teams,
+          and remove Pokémon from your teams.
+        </p>
+        <b>Instructions</b>
+        <ol>
+          <li>
+            Click the 'Edit' button and select an empty space to add a Pokémon
+            to your team.
+          </li>
+          <li>Use the search bar to find a Pokémon by name.</li>
+          <li>
+            Click the 'Add' button to add the selected Pokémon to your team.
+          </li>
+        </ol>
 
-      {isLoading && <p>Loading teams...</p>}
+        <b>Delete Instructions</b>
+        <ol>
+          <li>
+            Click the 'Edit' button and select a Pokémon to remove from your
+            team.
+          </li>
+          <li>Click the 'Remove' button to remove the selected Pokémon.</li>
+        </ol>
+        
+        {visible && (
+          <>
+            <div className="edit-pokemon">
+              <h3>Pokemon</h3>
+              <form
+                className="search-form"
+                onSubmit={(event) => handleSearchSubmit(event)}
+              >
+                <input
+                  type="text"
+                  placeholder="Search Pokemon..."
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                />
+                <button type="submit"> Search</button>
+              </form>
+              <span>
+                {editablePokemon.name ? editablePokemon.name : "Pokemon Name"}
+              </span>
+              <img
+                src={editablePokemon.image ? editablePokemon.image : imgholder}
+                alt="Pokemon"
+              />
+            </div>
+          </>
+        )}
+      </div>
 
-      {error && <p className="error">{error}</p>}
+      {isLoading && (
+        <div
+          style={{ display: "flex", justifyContent: "center", margin: "28%" }}
+        >
+          <Loader />
+        </div>
+      )}
 
       {teams.length > 0 && (
-        teams.map((team, index) => (
-          <div key={index}>
-            <p>Team ID: {team.id_equipo}</p>
-            <p>User ID: {team.id_usuario}</p>
-            <h2>Team Pokemon</h2>
-            <ul>
-              {team.pokemons.map((pokemon) => (
-                <li key={pokemon.nombre}>
-                  <p>{pokemon.nombre}</p>
-                  <img src={pokemon.imagen} alt={pokemon.nombre} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))
+        <div className="pokemon-team-gallery">
+          {teams.map((team, index) => (
+            <div className="pokemon-team-container" key={index}>
+              <div className="head-pokemon-team">
+                <h2 className="titles">Team Pokemon</h2>
+              </div>
+              <ul className="pokemon-team">
+                {team.pokemons.map((pokemon, index) => (
+                  <li className="pokemon" key={index}>
+                    <p style={{ fontWeight: 600 }}>
+                      {pokemon.name ? formatString(pokemon.name) : "No Pokemon"}
+                    </p>
+                    <img
+                      src={pokemon.image ? pokemon.image : imgholder}
+                      alt={pokemon.name}
+                    />
+                    {visible && (
+                      <span
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleEditPokemon(pokemon, index)}
+                      >
+                        <TbPencilCog />
+                      </span>
+                    )}
+                  </li>
+                ))}
+              </ul>
+              <div className="team-options" key={index}>
+                <p style={{ textAlign: "center" }}> Options</p>
+                <button
+                  style={{ backgroundColor: "green", fontWeight: 600 }}
+                  onClick={() =>
+                    handleUpdatePokemonOnTeam(
+                      team.id_equipo,
+                      editIndex,
+                      editablePokemon.id
+                    )
+                  }
+                >
+                  Add
+                </button>
+                <button
+                  style={{ backgroundColor: "blue", fontWeight: 600 }}
+                  onClick={() => setVisible(!visible)}
+                >
+                  Edit
+                </button>
+                <button
+                  style={{ fontWeight: 600 }}
+                  onClick={() =>
+                    handleDeletePokemonFromTeam(team.id_equipo, editIndex)
+                  }
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -81,26 +81,28 @@ app.post("/register", (req, res) => {
   );
 });
 
+//PKM TEAMS ROUTES
 //GET PKM Teams
-app.get('/team', (req, res) => {
+app.get("/team", (req, res) => {
   const { username } = req.query;
 
   if (!username) {
-    return res.status(400).send('Username parameter is required');
+    return res.status(400).send("Username parameter is required");
   }
 
-  // Updated query with LEFT JOINs for all pokemon
+  // Updated query with LEFT JOINs for all pokemon and added id_pokemon selections
   const query = `
     SELECT
       u.nickname,
-      p1.nombre AS pokemon1_nombre, p1.imagen AS pokemon1_imagen,
-      p2.nombre AS pokemon2_nombre, p2.imagen AS pokemon2_imagen,
-      p3.nombre AS pokemon3_nombre, p3.imagen AS pokemon3_imagen,
-      p4.nombre AS pokemon4_nombre, p4.imagen AS pokemon4_imagen,
-      p5.nombre AS pokemon5_nombre, p5.imagen AS pokemon5_imagen,
-      p6.nombre AS pokemon6_nombre, p6.imagen AS pokemon6_imagen
+      ep.id_equipo,
+      p1.id_pokemon AS pokemon1_id, p1.nombre AS pokemon1_nombre, p1.imagen AS pokemon1_imagen,
+      p2.id_pokemon AS pokemon2_id, p2.nombre AS pokemon2_nombre, p2.imagen AS pokemon2_imagen,
+      p3.id_pokemon AS pokemon3_id, p3.nombre AS pokemon3_nombre, p3.imagen AS pokemon3_imagen,
+      p4.id_pokemon AS pokemon4_id, p4.nombre AS pokemon4_nombre, p4.imagen AS pokemon4_imagen,
+      p5.id_pokemon AS pokemon5_id, p5.nombre AS pokemon5_nombre, p5.imagen AS pokemon5_imagen,
+      p6.id_pokemon AS pokemon6_id, p6.nombre AS pokemon6_nombre, p6.imagen AS pokemon6_imagen
     FROM users AS u
-    INNER JOIN equipo_pokemon AS ep ON u.nickname = ep.id_usuario
+    INNER JOIN equipo_pokemon AS ep ON u.id = ep.id_usuario
     LEFT JOIN pokemon AS p1 ON ep.pokemon1_id = p1.id_pokemon
     LEFT JOIN pokemon AS p2 ON ep.pokemon2_id = p2.id_pokemon
     LEFT JOIN pokemon AS p3 ON ep.pokemon3_id = p3.id_pokemon
@@ -114,7 +116,7 @@ app.get('/team', (req, res) => {
   connection.query(query, [username], (error, results, fields) => {
     if (error) {
       console.error(error);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
       res.json({
         teams: results.map((team) => ({
@@ -122,12 +124,36 @@ app.get('/team', (req, res) => {
           id_usuario: team.id_usuario,
           fecha_creacion: team.fecha_creacion,
           pokemons: [
-            { nombre: team.pokemon1_nombre, imagen: team.pokemon1_imagen },
-            { nombre: team.pokemon2_nombre, imagen: team.pokemon2_imagen },
-            { nombre: team.pokemon3_nombre, imagen: team.pokemon3_imagen },
-            { nombre: team.pokemon4_nombre, imagen: team.pokemon4_imagen },
-            { nombre: team.pokemon5_nombre, imagen: team.pokemon5_imagen },
-            { nombre: team.pokemon6_nombre, imagen: team.pokemon6_imagen },
+            {
+              id: team.pokemon1_id,
+              name: team.pokemon1_nombre,
+              image: team.pokemon1_imagen,
+            },
+            {
+              id: team.pokemon2_id,
+              name: team.pokemon2_nombre,
+              image: team.pokemon2_imagen,
+            },
+            {
+              id: team.pokemon3_id,
+              name: team.pokemon3_nombre,
+              image: team.pokemon3_imagen,
+            },
+            {
+              id: team.pokemon4_id,
+              name: team.pokemon4_nombre,
+              image: team.pokemon4_imagen,
+            },
+            {
+              id: team.pokemon5_id,
+              name: team.pokemon5_nombre,
+              image: team.pokemon5_imagen,
+            },
+            {
+              id: team.pokemon6_id,
+              name: team.pokemon6_nombre,
+              image: team.pokemon6_imagen,
+            },
           ],
         })),
       });
@@ -135,7 +161,138 @@ app.get('/team', (req, res) => {
   });
 });
 
+//UPDATE PKM Team
+app.put("/team", (req, res) => {
+  const { teamId, pokemonPosition, newPokemonId } = req.body;
 
+  //Verify if the required fields are present
+  if (!teamId || !pokemonPosition || !newPokemonId) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  // Update query to set the new pokemon id in the specified position
+  const query = `UPDATE equipo_pokemon SET pokemon${pokemonPosition}_id = ? WHERE id_equipo = ?`;
+
+  connection.query(query, [newPokemonId, teamId], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      // Verify if the query affected any rows
+      if (results.affectedRows > 0) {
+        res.json({ message: "Pokemon updated successfully" });
+      } else {
+        res.status(404).json({ message: "Team or Pokemon position not found" });
+      }
+    }
+  });
+});
+
+app.post("/team", (req, res) => {
+  const { username, team } = req.body;
+
+  if (!username || !team) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  // Insert query to add a new team
+  const query = `INSERT INTO equipo_pokemon (id_usuario, pokemon1_id, pokemon2_id, pokemon3_id, pokemon4_id, pokemon5_id, pokemon6_id) VALUES (
+    (SELECT id FROM users WHERE nickname = ?),
+    ?, ?, ?, ?, ?, ?
+  )`;
+
+  connection.query(
+    query,
+    [username, team[0], team[1], team[2], team[3], team[4], team[5]],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+      } else {
+        res.json({ message: "Team created successfully" });
+      }
+    }
+  );
+});
+
+app.delete("/team", (req, res) => {
+  const { teamId } = req.body;
+
+  if (!teamId) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  // Delete query to remove a team by its id
+  const query = `DELETE FROM equipo_pokemon WHERE id_equipo = ?`;
+
+  connection.query(query, [teamId], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      // Verify if the query affected any rows
+      if (results.affectedRows > 0) {
+        res.json({ message: "Team deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Team not found" });
+      }
+    }
+  });
+});
+
+//Pokemon BD Local ROUTES
+
+app.get("/pokemon", (req, res) => {
+  const id = req.query.id;
+  let query = `SELECT * FROM pokemon`;
+
+  if (id) {
+    query = `SELECT * FROM pokemon WHERE id_pokemon = ?`;
+  }
+
+  connection.query(query, [id], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      res.json(results);
+    }
+  });
+});
+app.post("/pokemon", (req, res) => {
+  const { id, name, image } = req.body;
+  if (!id || !name || !image) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  const query = `INSERT INTO pokemon (id_pokemon, nombre, imagen) VALUES (?, ?, ?)`;
+
+  connection.query(query, [id, name, image], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      res.json({ message: "Pokemon created successfully" });
+    }
+  });
+});
+
+app.put("/team/pokemon", (req, res) => {
+  const { teamId, pokemonPosition } = req.body;
+  if (!teamId || !pokemonPosition) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  const query = `UPDATE equipo_pokemon SET pokemon${pokemonPosition}_id = NULL WHERE id_equipo = ?`;
+  connection.query(query, [teamId], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      res.json({ message: "Pokemon deleted successfully" });
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`API REST escuchando en el puerto ${port}`);
